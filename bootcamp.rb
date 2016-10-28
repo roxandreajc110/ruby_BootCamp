d630913d7b3fa201daf2beac308045ce5090a8f3
Roxana Jaramillo <roxandreajc110@hotmail.com>
2016-10-27 22:02:14 -0500

    Ejercicio en Ruby con Clases

---
+++ bootcamp.rb
@@ -0,0 +1,11 @@
+require "./instructor.rb"
+
+instructor1 = Instructor.new({
+  first_name: "Yonga",
+  nickname: "yg"
+  })
+puts instructor1  
+puts "Los datos son #{instructor1.first_name} #{instructor1.last_name} #{instructor1.nickname}"
+
+# instructor2 = Instructor.new
+# puts "Los datos son #{instructor2.first_name} #{instructor2.last_name} #{instructor2.nickname}"
